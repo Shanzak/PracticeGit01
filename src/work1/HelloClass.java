5d88750eb2409b0d527fcc0e9da0daf8c1fb6890
shanzakhan <skhan153@icloud.com>
2022-01-17 19:40:14 -0500

    added more work

--- src/work1/HelloClass.java
+++ src/work1/HelloClass.java
@@ -3,6 +3,7 @@ package work1;
 public class HelloClass {
     public static void main(String[] args) {
         System.out.println("Work1 Started");
+        System.out.println("created Git Repository");
 
     }
 
